ab523889c5f5859afc98c915cac6c542155bfc70
code-matheus <matheusaugustodasilva2002@gmail.com>
2024-07-19 21:09:35 -0300

    finish api

--- server.js
+++ server.js
@@ -7,10 +7,61 @@ const prisma = new PrismaClient();
 const server = express()
 server.use(express.json())
 
-const users = []
 
-server.post('/users', (req, res) => {
-    prisma
+server.post('/users', async (req, res) => {
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).send('User created');
+})
+
+server.put('/users/:id', async (req, res) => {
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(204).send('User updated');
+})
+
+
+server.delete('/users/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.status(200).send('User deleted');
+})
+
+
+server.get('/users', async (req, res) => {
+    let users = []
+
+    if (req.query) {
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name
+            }
+        }) 
+    }else {
+        users = await prisma.user.findMany()
+    }
+
+   await prisma.user.findMany()
+
+    res.status(200).json(users)
 })
 
 
